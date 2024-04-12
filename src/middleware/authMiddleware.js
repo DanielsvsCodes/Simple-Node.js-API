747850f2213165e48bf4ea751cf210f1c7147409
Daniel Silva <x358981@bcpcorp.net>
2024-04-12 17:50:15 +0100

    Fix - v.0.1

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  console.log(token);
+  if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Forbidden' });
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
